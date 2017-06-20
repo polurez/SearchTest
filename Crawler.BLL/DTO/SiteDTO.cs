5eebfc4cffbe973110cf9af6896488b6f5210388
Igor <polurez@rambler.ru>
2017-06-20 16:26:23 +0300

    Add DTO

---
+++ Crawler.BLL/DTO/SiteDTO.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Crawler.BLL.DTO
+{
+    public class SiteDTO
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
--- Crawler.BLL/Interfaces/ISearcher.cs
+++ Crawler.BLL/Interfaces/ISearcher.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Crawler.BLL.DTO;
+using System.Collections.Generic;
 
 namespace Crawler.BLL.Interfaces
 {
     public interface ISearcher
     {
-       Dictionary<string, List<string>> Find(string searchString);
-        //List<string> Find(string searchString);
-
-
+     List<SiteDTO> Find(string searchString);
+        
 
         string Add(string url, string userId);
     }
